5b9237b3c00de210457268d9c644e86618baa98b
Sam Kemp <samrowankemp@gmail.com>
2023-06-13 18:28:38 +0100

    last commit this evening

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 "use strict"
 
+let myName = prompt("Is my name Sam? Please answer yes/no or y/n only").toLowerCase();
+
+while (myName != "yes" && myName != "no" && myName != "y" && myName != "n") {
+  alert("Please answer yes/no or y/n only");
+  myName = prompt("Is my name Sam? Please answer yes/no or y/n only").toLowerCase();
+}
+
+if (myName === "yes" || myName === "y") {
+    console.log("Correct, my name is Sam");
+  alert("Correct, my name is Sam");
+} else if (myName === "no" || myName === "n") {
+    console.log("That is not correct");
+  alert("That is not correct");
+}
