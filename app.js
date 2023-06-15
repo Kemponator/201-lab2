f2c39282fda1e56b2021407f79c044bb99903090
Sam Kemp <samrowankemp@gmail.com>
2023-06-15 16:11:29 +0100

    Updates for question 7 and also pair programming with Nick

--- app.js
+++ app.js
@@ -1,110 +1,138 @@
-"use strict"
+"use strict";
 
 console.log("Hello");
 
 //var yourName = prompt("What is your name?"); {
-  //console.log("Nice to meet you, " + yourName);
-  //alert("Nice to meet you," + yourName);
+//console.log("Nice to meet you, " + yourName);
+//alert("Nice to meet you," + yourName);
 //}
 
 //if (yourName === Response) {
-  //console.log("Nice to meet you, " + yourName);
-  //alert("Nice to meet you," + yourName);
+//console.log("Nice to meet you, " + yourName);
+//alert("Nice to meet you," + yourName);
 //}
 
-
-let myName = prompt("Is my name Sam? Please answer yes/no or y/n only").toLowerCase();
+let myName = prompt(
+  "Is my name Sam? Please answer yes/no or y/n only"
+).toLowerCase();
 
 while (myName != "yes" && myName != "no" && myName != "y" && myName != "n") {
   alert("Please answer yes/no or y/n only");
-  myName = prompt("Is my name Sam? Please answer yes/no or y/n only").toLowerCase();
+  myName = prompt(
+    "Is my name Sam? Please answer yes/no or y/n only"
+  ).toLowerCase();
 }
 
 if (myName === "yes" || myName === "y") {
-    console.log("Correct, my name is Sam");
+  console.log("Correct, my name is Sam");
   alert("Correct, my name is Sam");
 } else if (myName === "no" || myName === "n") {
-    console.log("That is not correct");
+  console.log("That is not correct");
   alert("That is not correct");
 }
 
-let myStudy = prompt("Did I study Media in college and University? Please answer yes/no or y/n").toLowerCase();
+let myStudy = prompt(
+  "Did I study Media in college and University? Please answer yes/no or y/n"
+).toLowerCase();
 
-while (myStudy != "yes" && myStudy != "no" && myStudy != "y" && myStudy != "n") {
-  alert("Try again!")
-  myStudy = prompt("Did I study Media in college and University? Please answer yes/no or y/n").toLowerCase();
+while (
+  myStudy != "yes" &&
+  myStudy != "no" &&
+  myStudy != "y" &&
+  myStudy != "n"
+) {
+  alert("Try again!");
+  myStudy = prompt(
+    "Did I study Media in college and University? Please answer yes/no or y/n"
+  ).toLowerCase();
 }
 
 if (myStudy === "yes" || myStudy === "y") {
-   console.log("Well done! I did study Media");
+  console.log("Well done! I did study Media");
   alert("Well done - I did study Media");
 } else if (myStudy === "no" || myStudy === "n") {
-   console.log("Try that again!");
-  alert("Try that again!")
+  console.log("Try that again!");
+  alert("Try that again!");
 }
 
 let myBirthday = prompt("Is my birthday in 2002?").toLowerCase();
 
-while (myBirthday != "yes" && myBirthday != "no" && myBirthday != "y" && myBirthday != "n") {
+while (
+  myBirthday != "yes" &&
+  myBirthday != "no" &&
+  myBirthday != "y" &&
+  myBirthday != "n"
+) {
   alert("Please answer yes/no or y/n only");
   myBirthday = prompt("Is my birthday in 2002?").toLowerCase();
 }
 
 if (myBirthday === "yes" || myBirthday === "y") {
-    console.log("Incorrect, it's in 1994 (I'm ancient)");
+  console.log("Incorrect, it's in 1994 (I'm ancient)");
   alert("Incorrect, it's in 1994 (I'm ancient)");
 } else if (myBirthday === "no" || myBirthday === "n") {
-    console.log("Correct, I'm ancient!");
-  alert("Correct, I'm ancient!")
+  console.log("Correct, I'm ancient!");
+  alert("Correct, I'm ancient!");
 }
 
-let myFood = prompt("Is my favourite food sushi? Please answer yes/no or y/n only").toLowerCase();
+let myFood = prompt(
+  "Is my favourite food sushi? Please answer yes/no or y/n only"
+).toLowerCase();
 
 while (myFood != "yes" && myFood != "no" && myFood != "y" && myFood != "n") {
   alert("Please answer yes/no or y/n only");
-  myFood = prompt("Is my favourite food sushi? Please answer yes/no or y/n only").toLowerCase();
+  myFood = prompt(
+    "Is my favourite food sushi? Please answer yes/no or y/n only"
+  ).toLowerCase();
 }
 
 if (myFood === "yes" || myFood === "y") {
-    console.log("Correct, it's delicious!");
+  console.log("Correct, it's delicious!");
   alert("Correct, it's delicious!");
 } else if (myFood === "no" || myFood === "n") {
-    console.log("That is not correct");
+  console.log("That is not correct");
   alert("That is not correct");
 }
 
-let myFuture = prompt("Do I want to be an astronaut? Please answer yes/no or y/n only").toLowerCase();
+let myFuture = prompt(
+  "Do I want to be an astronaut? Please answer yes/no or y/n only"
+).toLowerCase();
 
-while (myFuture != "yes" && myFuture != "no" && myFuture != "y" && myFuture != "n") {
+while (
+  myFuture != "yes" &&
+  myFuture != "no" &&
+  myFuture != "y" &&
+  myFuture != "n"
+) {
   alert("Please answer yes/no or y/n only");
-  myFuture = prompt("Do I want to be an astronaut? Please answer yes/no or y/n only").toLowerCase();
+  myFuture = prompt(
+    "Do I want to be an astronaut? Please answer yes/no or y/n only"
+  ).toLowerCase();
 }
 
 if (myFuture === "yes" || myFuture === "y") {
-    console.log("Wrong, I want to be a developer");
+  console.log("Wrong, I want to be a developer");
   alert("Wrong, I want to be a developer");
 } else if (myFuture === "no" || myFuture === "n") {
-    console.log("That is correct - I want to be a software developer");
+  console.log("That is correct - I want to be a software developer");
   alert("That is correct - I want to be a software developer");
 }
 
 // Generated-number guessing game.
 
 function guessNumber() {
-
   const random = Math.floor(Math.random() * 10) + 1;
 
-  let number = parseInt(prompt('Guess a number from 1 to 10: '));
+  let number = parseInt(prompt("Guess a number from 1 to 10: "));
 
-  while(number !== random) {
-      number = parseInt(prompt('Guess a number from 1 to 10: '));
+  while (number !== random) {
+    number = parseInt(prompt("Guess a number from 1 to 10: "));
   }
 
-  if(number == random) {
-      console.log('You guessed the correct number.');
-      alert("You guessed the correct number!");
+  if (number == random) {
+    console.log("You guessed the correct number.");
+    alert("You guessed the correct number!");
   }
-
 }
 
 // My Process
@@ -132,35 +160,88 @@ function guessNumber() {
 // change the number variable to use Math.floor and Math.random to generate a random number.
 // declare a variable named guessNum and set the value to parseInt(guess)
 
-guessNumber();
+function guessNumber() {
+  // const number = "7";
+  const number = Math.floor(Math.random() * 10 + 1);
+  console.log(number);
 
-// const number = "7";
-const number = Math.floor(Math.random() * 10 + 1);
-console.log(number);
+  for (let i = 2; i >= 0; i--) {
+    let guesses = i + 1;
 
-for (let i = 2; i >= 0; i--) {
-  let guesses = i + 1;
+    let guess = prompt(
+      "Guess a number between 1 and 10! You have " + guesses + " attempts"
+    );
 
-  let guess = prompt("Guess a number between 1 and 10! you have " + guesses + " attempts");
+    const guessNum = parseInt(guess);
 
-  const guessNum = parseInt(guess);
+    if (guessNum === number) {
+      // console.log("Well done, you win!");
+      alert("Well done, you win!");
+      break;
+    } else if (guessNum < number) {
+      // console.log("Too low!");
+      alert("Too low!");
+    } else if (guessNum > number) {
+      // console.log("Too high!");
+      alert("Too high!");
+    }
 
-  if (guessNum === number) {
-    // console.log("You win");
-    alert("You win");
-    break;
-  } else if (guessNum < number) {
-    // console.log("to low");
-    alert("to low");
-  } else if (guessNum > number) {
-    // console.log("to high");
-    alert("to high");
-  }
-
-  if (i === 0) {
-    alert("The correct number was " + number);
-    break;
+    if (i === 0) {
+      alert("The correct number was " + number);
+      break;
+    }
   }
 }
 
-//const myArray = ("JimBob, JammyDodger, JezzaBell, JesusChrist")
+guessNumber();
+
+function guessFood() {
+  const answerFood = [
+    "Benedicts",
+    "Haggle",
+    "Trattoria Rustica",
+    "Shiki",
+    "Kimchi",
+    "Dhaba at Fifteen",
+    "Namaste Village",
+    "The Belgian Monk",
+    "Roger Hickman's",
+    "Don Txoko",
+  ];
+  console.log(answerFood);
+
+  for (let i = 5; i >= 0; i--) {
+    let guesses = i + 1;
+
+    let guessFoodPlace = prompt(
+      "Can you name one of my favourite places to eat in Norwich? You have " +
+        guesses +
+        " attempts"
+    ).toLowerCase();
+    console.log(i);
+  }
+
+  if (
+    guessFoodPlace == "Benedicts" ||
+    guessFoodPlace == "Haggle" ||
+    guessFoodPlace == "Trattoria Rustica" ||
+    guessFoodPlace == "Shiki" ||
+    guessFoodPlace == "Kimchi" ||
+    guessFoodPlace == "Dhaba at Fifteen" ||
+    guessFoodPlace == "Namaste Village" ||
+    guessFoodPlace == "The Belgian Monk" ||
+    guessFoodPlace == "Roger Hickman's" ||
+    guessFoodPlace == "Don Txoko"
+  ) {
+    alert("Well done, you guessed correct!");
+  } else {
+    alert("Incorrect - have another go!");
+
+    if (i === 0) {
+      alert("Sorry, you've run out of guesses! The answers were " + answerFood);
+    }
+    console.log(guesses);
+  }
+}
+
+guessFood();
